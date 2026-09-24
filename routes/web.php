40bf69f7bd3efb29d027b6d047b9021e6be54d28
rifki <rifkizwei5@gmail.com>
2026-09-24 10:01:39 +0700

    laravel

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('profil');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/anggota', function () {
+    return view('anggota');
+});
+
+Route::get('/kontak', function () {
+    return view('kontak');
+});
